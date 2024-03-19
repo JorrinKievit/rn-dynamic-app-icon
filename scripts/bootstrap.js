95b1b3a86736bc0880c1fb1cb66e41c11c164226
Jorrin <jorrinkievit@hotmail.com>
2024-03-19 21:09:15 +0100

    test

--- scripts/bootstrap.js
+++ scripts/bootstrap.js
@@ -20,10 +20,10 @@ let result;
 if (process.cwd() !== root || args.length) {
   // We're not in the root of the project, or additional arguments were passed
   // In this case, forward the command to `yarn`
-  result = child_process.spawnSync('yarn', args, options);
+  result = child_process.spawnSync('pnpm', args, options);
 } else {
   // If `yarn` is run without arguments, perform bootstrap
-  result = child_process.spawnSync('yarn', ['bootstrap'], options);
+  result = child_process.spawnSync('pnpm', ['bootstrap'], options);
 }
 
 process.exitCode = result.status;
